4ec9924f3493b44ddd5783ea7672e561873b713d
Colby Jensen <endergdawg@gmail.com>
2024-04-08 09:52:48 -0600

    Add email type

--- email.py
+++ email.py
@@ -1,5 +1,31 @@
 class Email:
     def __init__(self, content: str):
+        if not isinstance(content, str):
+            raise TypeError("please provide a string")
+        
+        if "@" not in content:
+            raise TypeError("Doesn't have an '@' sign")
+        
+        # Ends with .com/ .org/ .edu/ .org
+        # TLD -> Top Level Domain, which is the com/org/edu/etc.
+        vaild_tlds: list[str] = [".com", ".edu", ".net", ".org"]
+
+        tld = content[-4:]
+        if not content.endswith(".com"):
+            raise TypeError("Invalid TLD")
+        
+
+        # Check for domain (stuff between @ and .com)
+        at_sign_index = content.find("@") + 1
+        domain = content[at_sign_index:-4]
+        if domain = "":
+            raise TyperError("Invalid Domain")
+
+        #Check for stuff before the @'
+        if content.find("@") == 0
+            raise TypeError("Invalid User")
+
+
         self.content = content
 
     def __str__(self) -> str:
